97865ad8be8da6d60a12b67e6008353f6d89a4b3
artem224 <72102981+artem224@users.noreply.github.com>
2021-05-26 18:58:43 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
                 </Route>
                 <Route path="/writers">
                     <Writers/>
-                    <newdata/>
                 </Route>
             </Switch>
         </Router>
