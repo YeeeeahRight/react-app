f1dcba945d7455b4ed5e90e8ff7f996eaf339955
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-05-30 04:36:48 +0300

    changes for gh-pages

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -3,11 +3,25 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector'
 import {initReactI18next} from 'react-i18next';
 
+import translationEN from './locales/en/translation.json';
+import translationRU from './locales/ru/translation.json';
+
+// the translations
+const resources = {
+    en: {
+        translation: translationEN
+    },
+    ru: {
+        translation: translationRU
+    }
+};
+
 i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        resources,
         fallbackLng: 'ru',
         debug: true,
         // Распознавание и кэширование языковых кук
